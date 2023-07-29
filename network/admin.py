b609778de528acd784e112bab00c3c9301c9197c
helington <helington27@gmail.com>
2023-07-29 09:15:51 -0300

    Add super user to django project

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import User, Post, Comment, Following_List, Post_Feedback
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Following_List)
+admin.site.register(Post_Feedback)
