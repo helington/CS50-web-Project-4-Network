a4a21a0591f18554794a102de8c185ecc043c1f6
helington <helington27@gmail.com>
2023-07-29 09:17:52 -0300

    Make views and responses for API routes

--- network/views.py
+++ network/views.py
@@ -1,12 +1,13 @@
 import json
 from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post
 
 
 def index(request):
@@ -64,14 +65,60 @@ def register(request):
     else:
         return render(request, "network/register.html")
 
+
 def newPost(request):
-    pass
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+
+    data = json.loads(request.body)
+    author = request.user
+    content = data.get("content", "")
+
+    post = Post(
+        author=author,
+        content=content
+    )
+
+    post.save()
+    return JsonResponse({"message": "Post registred succesfully."}, status=201)
 
 def allPosts(request):
-    pass
+    posts = Post.objects.all().order_by("-timestamp")
+    return JsonResponse([post.serialize() for post in posts], safe=False)
 
-def filteredUsers(request):
-    pass
+def postById(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        return JsonResponse({"error": "Post not found."}, status=404)
 
-def userInfo(request):
-    pass
+@login_required
+def followingPosts(request):
+    posts = []
+    user = request.user
+    following_users = user.following_list.following.all()
+    for following_user in following_users:
+        for post in following_user.posts.all():
+            posts.append(post.serialize())
+    
+    return JsonResponse(posts, safe=False)
+
+def userInfo(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return JsonResponse({"error": "User doens't exist."}, status=404)
+
+    if request.method == "GET":
+        return JsonResponse(user.serialize())
+    
+    elif request.method == "PUT":
+        data = json.loads(request.body)
+        if data.get("followers") is not None:
+            user.followers = data["followers"]
+        user.save()
+        return HttpResponse(status=204)
+    else:
+        return JsonResponse({
+            "error": "GET or PUT request required."
+        }, status=400)
